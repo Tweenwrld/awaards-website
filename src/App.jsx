e82916c7dea007f0b43623340b200b410cbcc134
Tweenwrld <154606522+Tweenwrld@users.noreply.github.com>
2025-09-23 10:02:33 +0300

    feat(app): compose main layout with Hero, Navbar, About, Features, Story, Contact, and Footer components

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import About from "./components/About";
+import Features from './components/Features';
+import Story from './components/Story'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <main className='relative min-h-screen w-screen overflow-x-hidden bg-zinc-600'>
+      <Hero />
+      <Navbar />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
